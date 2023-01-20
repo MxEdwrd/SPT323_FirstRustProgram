292ab33dc8ec749dcbe5b34447bce4ece0071ca3
Max <84058507+MxEdwrd@users.noreply.github.com>
2023-01-19 17:03:50 -0700

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,9 @@
+fn main() { //Start of the "main" function
+	println!(); //A blank print line, followed by more printed lines
+	println!("--------------------");
+    println!("Max Edward | 1/12/23 \n This is a short list of Rust Keywords! \n 
+break: Break is used to get out of a loop as soon as the line where the break command is reached. \n
+fn: 'fn' is used to create a function, similar to something like 'void' in Arduino. \n
+let: Let defines a variable, let is used to say make the letter 'x' equal to the number '1'. \n");
+	println!("--------------------");
+} //End of the function
